69196d746f810fbbf107f0caa8373bc0014205d0
MSCapabilities <47613615+MSCapabilities@users.noreply.github.com>
2019-02-27 09:50:43 +1300

    Update BackEndApplication.py

--- Quickstarts/back-end-application/BackEndApplication.py
+++ Quickstarts/back-end-application/BackEndApplication.py
@@ -1,33 +1,21 @@
 import sys
-
-# Copyright (c) Microsoft. All rights reserved.
-# Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-# Using the Python Device SDK for IoT Hub:
-#   https://github.com/Azure/azure-iot-sdk-python
-# The sample connects to a device-specific MQTT endpoint on your IoT Hub.
 import iothub_service_client
-# pylint: disable=E0611
 from iothub_service_client import IoTHubDeviceMethod, IoTHubError
 from builtins import input
 
-# The service connection string to authenticate with your IoT hub.
-# Using the Azure CLI:
-# az iot hub show-connection-string --hub-name {your iot hub name}
-CONNECTION_STRING = "{Your IoT hub service connection string}"
-DEVICE_ID = "MyPythonDevice"
 
-# Details of the direct method to call.
+CONNECTION_STRING = "{Your IoT hub service connection string}"
+DEVICE_ID = "RaspberryPi"
+
+
 METHOD_NAME = "SetTelemetryInterval"
 METHOD_PAYLOAD = "10"
 TIMEOUT = 60
 
 def iothub_devicemethod_sample_run():
     try:
-        # Connect to your hub.
         iothub_device_method = IoTHubDeviceMethod(CONNECTION_STRING)
 
-        # Call the direct method.
         response = iothub_device_method.invoke(DEVICE_ID, METHOD_NAME, METHOD_PAYLOAD, TIMEOUT)
 
         print ( "" )
