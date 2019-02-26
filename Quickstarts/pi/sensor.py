d18413290a9795d4e0a8902eca85804d4f842ad6
MSCapabilities <47613615+MSCapabilities@users.noreply.github.com>
2019-02-27 09:47:56 +1300

    Update sensor.py

--- Quickstarts/pi/sensor.py
+++ Quickstarts/pi/sensor.py
@@ -5,10 +5,17 @@ CONNECTION_STRING = "<YOUR CONNECTION STRING HERE>"
 
 
 
-import random
+
+
+
+
+
+
 import time
 import sys
-
+import iothub_client
+from iothub_client import IoTHubClient, IoTHubClientError, IoTHubTransportProvider, IoTHubClientResult
+from iothub_client import IoTHubMessage, IoTHubMessageDispositionResult, IoTHubError, DeviceMethodReturnValue
 from sense_hat import SenseHat
 from time import sleep
 
@@ -27,33 +34,10 @@ b = 255
 sense.clear((r, g, b))
 sleep(1)
 
-pressure = sense.get_pressure()
-data = str(pressure)
 
-temp = str(sense.get_temperature())
-
-hum = str(sense.get_humidity())
-
-
-# Using the Python Device SDK for IoT Hub:
-#   https://github.com/Azure/azure-iot-sdk-python
-# The sample connects to a device-specific MQTT endpoint on your IoT Hub.
-import iothub_client
-# pylint: disable=E0611
-from iothub_client import IoTHubClient, IoTHubClientError, IoTHubTransportProvider, IoTHubClientResult
-from iothub_client import IoTHubMessage, IoTHubMessageDispositionResult, IoTHubError, DeviceMethodReturnValue
-
-# The device connection string to authenticate the device with your IoT hub.
-# Using the Azure CLI:
-# az iot hub device-identity show-connection-string --hub-name msc-hub --device-id raspPi --output table
-
-# Using the MQTT protocol.
 PROTOCOL = IoTHubTransportProvider.MQTT
 MESSAGE_TIMEOUT = 10000
 
-# Define the JSON message to send to IoT Hub.
-TEMPERATURE = sense.get_temperature()
-HUMIDITY = sense.get_humidity()
 
 MSG_TXT = "{\"temperature\": %.2f,\"humidity\": %.2f}"
 
@@ -61,7 +45,6 @@ def send_confirmation_callback(message, result, user_context):
     print ( "IoT Hub responded to message with status: %s" % (result) )
 
 def iothub_client_init():
-    # Create an IoT Hub client
     client = IoTHubClient(CONNECTION_STRING, PROTOCOL)
     return client
 
@@ -72,35 +55,33 @@ def iothub_client_telemetry_sample_run():
         print ( "IoT Hub device sending periodic messages, press Ctrl-C to exit" )
 
         while True:
-            # Build the message with telemetry values from the sense HAT.
+   
             temperature = sense.get_temperature()
             humidity = sense.get_humidity()
             msg_txt_formatted = MSG_TXT % (temperature, humidity)
             message = IoTHubMessage(msg_txt_formatted)
 
-            # Add a custom application property to the message.
-            # An IoT hub can filter on these properties without access to the message body.
             prop_map = message.properties()
             if temperature > 30:
               prop_map.add("temperatureAlert", "true")
             else:
               prop_map.add("temperatureAlert", "false")
-
-            # Send the message.
+	
+	
             print( "Sending message: %s" % message.get_string() )
             client.send_event_async(message, send_confirmation_callback, None)
-	    sense.show_message(temp + " C")
+	    sense.show_message(temperature + " C")
 	    time.sleep(1)
-	    sense.show_message(hum + "%")
+	    sense.show_message(humidity + "%")
             time.sleep(2)
 
     except IoTHubError as iothub_error:
-        print ( "Unexpected error %s from IoTHub" % iothub_error )
+        print ( "Unexpected error %s from IoT Hub" % iothub_error )
         return
     except KeyboardInterrupt:
         print ( "IoTHubClient sample stopped" )
 
 if __name__ == '__main__':
-    print ( "IoT Hub Quickstart #1 - Simulated device" )
+    print ( "IoT Hub Sense HAT" )
     print ( "Press Ctrl-C to exit" )
     iothub_client_telemetry_sample_run()
